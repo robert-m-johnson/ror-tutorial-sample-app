a6cccfe99ba01eacdc564599c0aeab866fc76092
Robert Johnson <robert.m.johnson88@gmail.com>
2023-01-13 16:51:54 +0000

    Add initial valid user to user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+  end
+
+  test 'default user should be valid' do
+    assert @user.valid?
+  end
 end
