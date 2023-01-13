640e07652e9a7c9333257d6b88304a9b6e020ac7
Robert Johnson <robert.m.johnson88@gmail.com>
2023-01-13 15:35:06 +0000

    Initial placeholder content for signup page

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -5,5 +5,8 @@ class HomePageTest < ActionDispatch::IntegrationTest
     get root_path
 
     assert_select 'a[href=?]', signup_path
+
+    get signup_path
+    assert_select 'title', full_title('Sign up')
   end
 end
