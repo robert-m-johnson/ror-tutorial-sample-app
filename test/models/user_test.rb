a056d11c1e524a59eb5d522ed0516723c3606f87
Robert Johnson <robert.m.johnson88@gmail.com>
2023-01-17 08:13:41 +0000

    Add secure password to User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(
+      name: 'Example User',
+      email: 'user@example.com',
+      password: 'foobar',
+      password_confirmation: 'foobar')
   end
 
   test 'default user should be valid' do
